cb5612e74ec7c1d00c16e744190c1ff7e7cf6c2b
sjanuary <sjanuary@uk.ibm.com>
2017-07-19 14:38:31 +0100

    Add apmetrics graphs to graphmetrics

--- js/httpThroughPutChart.js
+++ js/httpThroughPutChart.js
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2017 IBM Corp.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -23,6 +23,8 @@ var httpDiv2GraphWidth = httpDiv2CanvasWidth - margin.left - margin.right;
 // set the scale dimensions to the size of the graph
 var httpTP_xScale = d3.time.scale().range([0, httpDiv2GraphWidth]);
 var httpTP_yScale = d3.scale.linear().range([tallerGraphHeight, 0]);
+
+var httpRate = [];
 
 // x axis format
 var httpTP_xAxis = d3.svg.axis().scale(httpTP_xScale)
@@ -182,4 +184,3 @@ function resizeHttpThroughputChart() {
     selection.select(".yAxis")
         .call(httpTP_yAxis);
 }
-
