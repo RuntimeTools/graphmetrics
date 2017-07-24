f0b03a43b2e897d846610788312c07a7d2436272
MattColegate <colegate@uk.ibm.com>
2017-07-24 13:45:06 +0100

    Fixing lint errors; set up Node framework

--- js/eventLoopChart.js
+++ js/eventLoopChart.js
@@ -19,7 +19,7 @@
 // Line chart for displaying event loop latency
 
 // Width of div allocated for this graph
-var eventLoopCanvasWidth = $("#eventLoopDiv").width() - 8; // -8 for margin and
+var eventLoopCanvasWidth = $('#eventLoopDiv').width() - 8; // -8 for margin and
 // border
 var eventLoopGraphWidth = eventLoopCanvasWidth - margin.left - margin.right;
 
@@ -32,12 +32,12 @@ var elData = [];
 
 // set up X axis for time in HH:MM:SS
 var el_xAxis = d3.svg.axis().scale(el_xScale)
-    .orient("bottom").ticks(3).tickFormat(getTimeFormat());
+    .orient('bottom').ticks(3).tickFormat(getTimeFormat());
 
 // set up Y axis for time in ms
 var el_yAxis = d3.svg.axis().scale(el_yScale)
-    .orient("left").ticks(8).tickFormat(function(d) {
-      return d + "ms";
+    .orient('left').ticks(8).tickFormat(function(d) {
+      return d + 'ms';
     });
 
 // line function for maximum latency
@@ -67,256 +67,261 @@ var el_avg_line = d3.svg.line()
       return el_yScale(d.latency.avg);
     });
 
-var elSVG = d3.select("#eventLoopDiv")
-    .append("svg")
-    .attr("width", eventLoopCanvasWidth)
-    .attr("height", canvasHeight)
-    .attr("class", "elChart");
+var elSVG = d3.select('#eventLoopDiv')
+    .append('svg')
+    .attr('width', eventLoopCanvasWidth)
+    .attr('height', canvasHeight)
+    .attr('class', 'elChart');
 
-var elTitleBox = elSVG.append("rect")
-    .attr("width", eventLoopCanvasWidth)
-    .attr("height", 30)
-    .attr("class", "titlebox");
+var elTitleBox = elSVG.append('rect')
+    .attr('width', eventLoopCanvasWidth)
+    .attr('height', 30)
+    .attr('class', "titlebox");
 
 // define the chart canvas
 var elChart = elSVG
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    .append('g')
+    .attr('transform',
+      'translate(' + margin.left + ',' + margin.top + ')');
 
 // Scale the X range to the data's time interval
 el_xScale.domain(d3.extent(elData, function(d) {
-  return d.time;
+    return d.time;
 }));
 
 // Scale the Y range from 0 to the largest maximum latency
 el_yScale.domain([0, Math.ceil(d3.extent(elData, function(d) {
-  return d.latency.max;
+    return d.latency.max;
 })[1] * 1000) / 1000]);
 
 // Draw the max line path.
-elChart.append("path")
-    .attr("class", "line1")
-    .attr("d", el_max_line(elData));
+elChart.append('path')
+    .attr('class', 'line1')
+    .attr('d', el_max_line(elData));
 
 // Draw the min line path.
-elChart.append("path")
-    .attr("class", "line2")
-    .attr("d", el_min_line(elData));
+elChart.append('path')
+    .attr('class', 'line2')
+    .attr('d', el_min_line(elData));
 
 // Draw the avg line path.
-elChart.append("path")
-    .attr("class", "line3")
-    .attr("d", el_avg_line(elData));
+elChart.append('path')
+    .attr('class', 'line3')
+    .attr('d', el_avg_line(elData));
 
 // Draw the X Axis
-elChart.append("g")
-    .attr("class", "xAxis")
-    .attr("transform", "translate(0," + graphHeight + ")")
+elChart.append('g')
+    .attr('class', 'xAxis')
+    .attr('transform', 'translate(0,' + graphHeight + ')')
     .call(el_xAxis);
 
 // Draw the Y Axis
-elChart.append("g")
-    .attr("class", "yAxis")
+elChart.append('g')
+    .attr('class', 'yAxis')
     .call(el_yAxis);
 
 // Draw the title
-elChart.append("text")
-    .attr("x", 7 - margin.left)
-    .attr("y", 15 - margin.top)
-    .attr("dominant-baseline", "central")
-    .style("font-size", "18px")
+elChart.append('text')
+    .attr('x', 7 - margin.left)
+    .attr('y', 15 - margin.top)
+    .attr('dominant-baseline', 'central')
+    .style('font-size', '18px')
     .text(object.eventLoopTitle);
 
 // Add the placeholder text
-var elChartPlaceholder = elChart.append("text")
-    .attr("x", eventLoopGraphWidth / 2)
-    .attr("y", graphHeight / 2)
-    .attr("text-anchor", "middle")
-    .style("font-size", "18px")
+var elChartPlaceholder = elChart.append('text')
+    .attr('x', eventLoopGraphWidth / 2)
+    .attr('y', graphHeight / 2)
+    .attr('text-anchor', 'middle')
+    .style('font-size', '18px')
     .text(object.NoDataMessage);
 
 // Add the MAXIMUM colour box
-elChart.append("rect")
-    .attr("x", 0)
-    .attr("y", graphHeight + margin.bottom - 15)
-    .attr("class", "colourbox1")
-    .attr("width", 10)
-    .attr("height", 10);
+elChart.append('rect')
+    .attr('x', 0)
+    .attr('y', graphHeight + margin.bottom - 15)
+    .attr('class', 'colourbox1')
+    .attr('width', 10)
+    .attr('height', 10);
 
 // Add the MAXIMUM line label
-var elMaxLabel = elChart.append("text")
-    .attr("x", 15)
-    .attr("y", graphHeight + margin.bottom - 5)
-    .attr("text-anchor", "start")
-    .attr("class", "lineLabel")
+var elMaxLabel = elChart.append('text')
+    .attr('x', 15)
+    .attr('y', graphHeight + margin.bottom - 5)
+    .attr('text-anchor', 'start')
+    .attr('class', 'lineLabel')
     .text(object.eventLoopMaximumMsg);
 
 // Add the MINIMUM colour box
-elChart.append("rect")
-    .attr("x", elMaxLabel.node().getBBox().width + 25)
-    .attr("y", graphHeight + margin.bottom - 15)
-    .attr("width", 10)
-    .attr("height", 10)
-    .attr("class", "colourbox2");
+elChart.append('rect')
+    .attr('x', elMaxLabel.node().getBBox().width + 25)
+    .attr('y', graphHeight + margin.bottom - 15)
+    .attr('width', 10)
+    .attr('height', 10)
+    .attr('class', 'colourbox2');
 
 // Add the MINIMUM line label
-var elMinLabel = elChart.append("text")
-    .attr("x", elMaxLabel.node().getBBox().width + 40)
-    .attr("y", graphHeight + margin.bottom - 5)
-    .attr("class", "lineLabel")
+var elMinLabel = elChart.append('text')
+    .attr('x', elMaxLabel.node().getBBox().width + 40)
+    .attr('y', graphHeight + margin.bottom - 5)
+    .attr('class', 'lineLabel')
     .text(object.eventLoopMinimumMsg);
 
 // Add the AVERAGE colour box
-elChart.append("rect")
-    .attr("x", elMaxLabel.node().getBBox().width + elMinLabel.node().getBBox().width + 50)
-    .attr("y", graphHeight + margin.bottom - 15)
-    .attr("width", 10)
-    .attr("height", 10)
-    .attr("class", "colourbox3");
+elChart.append('rect')
+    .attr('x', elMaxLabel.node().getBBox().width + elMinLabel.node().getBBox().width + 50)
+    .attr('y', graphHeight + margin.bottom - 15)
+    .attr('width', 10)
+    .attr('height', 10)
+    .attr('class', 'colourbox3');
 
 // Add the AVERAGE line label
-elChart.append("text")
-    .attr("x", elMaxLabel.node().getBBox().width + elMinLabel.node().getBBox().width + 65)
-    .attr("y", graphHeight + margin.bottom - 5)
-    .attr("class", "lineLabel")
+elChart.append('text')
+    .attr('x', elMaxLabel.node().getBBox().width + elMinLabel.node().getBBox().width + 65)
+    .attr('y', graphHeight + margin.bottom - 5)
+    .attr('class', 'lineLabel')
     .text(object.eventLoopAverageMsg);
 
 // Draw the Latest MAX Data
-elChart.append("text")
-    .attr("x", 0)
-    .attr("y", 0 - (margin.top * 3 / 8))
-    .attr("class", "maxlatest")
-    .style("font-size", "32px");
+elChart.append('text')
+    .attr('x', 0)
+    .attr('y', 0 - (margin.top * 3 / 8))
+    .attr('class', 'maxlatest')
+    .style('font-size', '32px');
 
 // Draw the Latest MIN Data
-elChart.append("text")
-    .attr("x", eventLoopGraphWidth / 3) // 1/3 across
-    .attr("y", 0 - (margin.top * 3 / 8))
-    .attr("class", "minlatest")
-    .style("font-size", "32px");
+elChart.append('text')
+    .attr('x', eventLoopGraphWidth / 3) // 1/3 across
+    .attr('y', 0 - (margin.top * 3 / 8))
+    .attr('class', 'minlatest')
+    .style('font-size', '32px');
 
 // Draw the Latest AVG Data
-elChart.append("text")
-    .attr("x", (eventLoopGraphWidth / 3) * 2) // 2/3 across
-    .attr("y", 0 - (margin.top * 3 / 8))
-    .attr("class", "avglatest")
-    .style("font-size", "32px");
-    var elChartIsFullScreen = false;
+elChart.append('text')
+    .attr('x', (eventLoopGraphWidth / 3) * 2) // 2/3 across
+    .attr('y', 0 - (margin.top * 3 / 8))
+    .attr('class', 'avglatest')
+    .style('font-size', '32px');
+
+var elChartIsFullScreen = false;
 
 // Add the maximise button
-var elResize = elSVG.append("image")
-    .attr("x", eventLoopCanvasWidth - 30)
-    .attr("y", 4)
-    .attr("width", 24)
-    .attr("height", 24)
-    .attr("xlink:href","graphmetrics/images/maximize_24_grey.png")
-    .attr("class", "maximize")
-    .on("click", function(){
-        elChartIsFullScreen = !elChartIsFullScreen
-        d3.selectAll(".hideable").classed("invisible", elChartIsFullScreen);
-        d3.select("#eventLoopDiv").classed("fullscreen", elChartIsFullScreen)
-            .classed("invisible", false); // remove invisible from this chart
-        if(elChartIsFullScreen) {
-            d3.select(".elChart .maximize").attr("xlink:href","graphmetrics/images/minimize_24_grey.png")
-            // Redraw this chart only
-            resizeEventLoopChart();
-        } else {
-            d3.select(".elChart .maximize").attr("xlink:href","graphmetrics/images/maximize_24_grey.png")
-            canvasHeight = 250;
-            graphHeight = canvasHeight - margin.top - margin.bottom;
-            // Redraw all
-            resize();
-        }
+var elResize = elSVG.append('image')
+    .attr('x', eventLoopCanvasWidth - 30)
+    .attr('y', 4)
+    .attr('width', 24)
+    .attr('height', 24)
+    .attr('xlink:href','graphmetrics/images/maximize_24_grey.png')
+    .attr('class', 'maximize')
+    .on('click', function(){
+      elChartIsFullScreen = !elChartIsFullScreen;
+      d3.selectAll('.hideable')
+        .classed('invisible', elChartIsFullScreen);
+      d3.select('#eventLoopDiv')
+        .classed('fullscreen', elChartIsFullScreen)
+        .classed('invisible', false); // remove invisible from this chart
+      if (elChartIsFullScreen) {
+        d3.select('.elChart .maximize')
+          .attr('xlink:href','graphmetrics/images/minimize_24_grey.png');
+        // Redraw this chart only
+        resizeEventLoopChart();
+      } else {
+        d3.select('.elChart .maximize')
+          .attr('xlink:href','graphmetrics/images/maximize_24_grey.png');
+        canvasHeight = 250;
+        graphHeight = canvasHeight - margin.top - margin.bottom;
+        // Redraw all
+        resize();
+      }
     })
-    .on("mouseover", function() {
-        if(elChartIsFullScreen) {
-            d3.select(".elChart .maximize").attr("xlink:href","graphmetrics/images/minimize_24.png")
-        } else {
-            d3.select(".elChart .maximize").attr("xlink:href","graphmetrics/images/maximize_24.png")
-        }
+    .on('mouseover', function() {
+      if (elChartIsFullScreen) {
+        d3.select('.elChart .maximize')
+          .attr('xlink:href','graphmetrics/images/minimize_24.png');
+      } else {
+        d3.select('.elChart .maximize')
+          .attr('xlink:href','graphmetrics/images/maximize_24.png');
+      }
     })
-    .on("mouseout", function() {
-        if(elChartIsFullScreen) {
-            d3.select(".elChart .maximize").attr("xlink:href","graphmetrics/images/minimize_24_grey.png")
-        } else {
-            d3.select(".elChart .maximize").attr("xlink:href","graphmetrics/images/maximize_24_grey.png")
-        }
+    .on('mouseout', function() {
+      if (elChartIsFullScreen) {
+        d3.select('.elChart .maximize')
+          .attr('xlink:href','graphmetrics/images/minimize_24_grey.png');
+      } else {
+        d3.select('.elChart .maximize')
+          .attr('xlink:href','graphmetrics/images/maximize_24_grey.png');
+      }
     });
 
 function resizeEventLoopChart() {
-    if(elChartIsFullScreen) {
-        canvasHeight = $("#eventLoopDiv").height() - 100;
-        graphHeight = canvasHeight - margin.top - margin.bottom;
+    if (elChartIsFullScreen) {
+      canvasHeight = $('#eventLoopDiv').height() - 100;
+      graphHeight = canvasHeight - margin.top - margin.bottom;
     }
-    eventLoopCanvasWidth = $("#eventLoopDiv").width() - 8;
+    eventLoopCanvasWidth = $('#eventLoopDiv').width() - 8;
     eventLoopGraphWidth = eventLoopCanvasWidth - margin.left - margin.right;
-
     // Redraw placeholder
     elChartPlaceholder
-        .attr("x", eventLoopGraphWidth / 2)
-        .attr("y", graphHeight / 2)
-
+      .attr('x', eventLoopGraphWidth / 2)
+      .attr('y', graphHeight / 2);
     // Move maximise/minimise button
-    elResize.attr("x", eventLoopCanvasWidth - 30).attr("y", 4)
-
+    elResize
+      .attr('x', eventLoopCanvasWidth - 30)
+      .attr('y', 4);
     // resize the canvas
-    var chart = d3.select(".elChart");
-    chart.attr("width", eventLoopCanvasWidth)
-        .attr("height", canvasHeight);
+    var chart = d3.select('.elChart');
+    chart
+      .attr('width', eventLoopCanvasWidth)
+      .attr('height', canvasHeight);
     // resize the scale and axes
     el_xScale = d3.time.scale().range([0, eventLoopGraphWidth]);
     el_xAxis = d3.svg.axis()
-        .scale(el_xScale)
-        .orient("bottom")
-        .ticks(3)
-        .tickFormat(getTimeFormat());
+      .scale(el_xScale)
+      .orient('bottom')
+      .ticks(3)
+      .tickFormat(getTimeFormat());
     el_yScale = d3.scale.linear().range([graphHeight, 0]);
     el_yAxis = d3.svg.axis().scale(el_yScale)
-        .orient("left")
-        .ticks(8)
-        .tickFormat(function(d) {
-            return d + "ms";
-        });
-
-    elTitleBox.attr("width", eventLoopCanvasWidth);
-
+      .orient('left')
+      .ticks(8)
+      .tickFormat(function(d) {
+        return d + 'ms';
+      });
+    elTitleBox
+      .attr('width', eventLoopCanvasWidth);
     el_xScale.domain(d3.extent(elData, function(d) {
         return d.time;
     }));
     el_yScale.domain([0, Math.ceil(d3.extent(elData, function(d) {
         return d.latency.max;
     })[1] * 1000) / 1000]);
-
     // update the data lines
-    chart.select(".line1")
-        .attr("d", el_max_line(elData));
-    chart.select(".line2")
-            .attr("d", el_min_line(elData));
-    chart.select(".line3")
-            .attr("d", el_avg_line(elData));
+    chart.select('.line1')
+      .attr('d', el_max_line(elData));
+    chart.select('.line2')
+      .attr('d', el_min_line(elData));
+    chart.select('.line3')
+      .attr('d', el_avg_line(elData));
     // update the axes
-    chart.select(".xAxis")
-        .attr("transform", "translate(0," + graphHeight + ")")
-        .call(el_xAxis);
-    chart.select(".yAxis")
-        .call(el_yAxis);
+    chart.select('.xAxis')
+      .attr('transform', 'translate(0,' + graphHeight + ')')
+      .call(el_xAxis);
+    chart.select('.yAxis')
+      .call(el_yAxis);
 
     // Move labels
-    chart.selectAll(".lineLabel")
-        .attr("y", graphHeight + margin.bottom - 5);
-    chart.select(".colourbox1")
-        .attr("y", graphHeight + margin.bottom - 15);
-    chart.select(".colourbox2")
-        .attr("y", graphHeight + margin.bottom - 15);
-    chart.select(".colourbox3")
-        .attr("y", graphHeight + margin.bottom - 15);
+    chart.selectAll('lineLabel')
+      .attr('y', graphHeight + margin.bottom - 5);
+    chart.select('.colourbox1')
+      .attr('y', graphHeight + margin.bottom - 15);
+    chart.select('.colourbox2')
+      .attr('y', graphHeight + margin.bottom - 15);
+    chart.select('.colourbox3')
+      .attr('y', graphHeight + margin.bottom - 15);
 }
 
 function updateEventLoopData(elRequest) {
-
     elRequestData = JSON.parse(elRequest);  // parses the data into a JSON array
-
     if (!elRequestData) return;
     var d = elRequestData;
     d.time = new Date(+d.time);
@@ -327,39 +332,36 @@ function updateEventLoopData(elRequest) {
     elData.push(d);
 
     if (elData.length === 2) {
-            // second data point - remove "No Data Available" label
-      elChartPlaceholder.attr("visibility", "hidden");
+      // second data point - remove "No Data Available" label
+      elChartPlaceholder.attr('visibility', 'hidden');
     }
-        // Only keep 'maxTimeWindow' (defined in index.html) milliseconds of data
+    // Only keep 'maxTimeWindow' (defined in index.html) milliseconds of data
     var currentTime = Date.now();
-    d = elData[0];
-    while (d.hasOwnProperty("time") && d.time + maxTimeWindow < currentTime) {
+    var first = elData[0];
+    while (first.hasOwnProperty('time') && first.time + maxTimeWindow < currentTime) {
       elData.shift();
-      d = elData[0];
+      first = elData[0];
     }
-
-        // Re-scale the X range to the new data time interval
+    // Re-scale the X range to the new data time interval
     el_xScale.domain(d3.extent(elData, function(d) {
       return d.time;
     }));
-        // Re-scale the Y range to the new largest max latency
+    // Re-scale the Y range to the new largest max latency
     el_yScale.domain([0, Math.ceil(d3.extent(elData, function(d) {
       return d.latency.max;
     })[1] * 1000) / 1000]);
-
     el_xAxis.tickFormat(getTimeFormat());
-
-    var selection = d3.select(".elChart");
-        // update the data lines
-    selection.select(".line1")
-            .attr("d", el_max_line(elData));
-    selection.select(".line2")
-            .attr("d", el_min_line(elData));
-    selection.select(".line3")
-            .attr("d", el_avg_line(elData));
-        // update the axes
-    selection.select(".xAxis")
-            .call(el_xAxis);
-    selection.select(".yAxis")
-            .call(el_yAxis);
+    var selection = d3.select('.elChart');
+    // update the data lines
+    selection.select('.line1')
+      .attr('d', el_max_line(elData));
+    selection.select('.line2')
+      .attr('d', el_min_line(elData));
+    selection.select('.line3')
+      .attr('d', el_avg_line(elData));
+    // update the axes
+    selection.select('.xAxis')
+      .call(el_xAxis);
+    selection.select('.yAxis')
+      .call(el_yAxis);
 }
