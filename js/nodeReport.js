2c51d1e7c75442378725bffd5b035654fdb00474
Matt Colegate <colegate@uk.ibm.com>
2017-08-02 08:22:10 +0100

    Enable automated testing (#9)
    * First pass for enabling automated testing
    
    * Fix structure problems in package.json
    
    * Add missing comma in package.json
    
    * Instruct lint to ignore 3rd party code
    
    * Fixing lint errors; set up Node framework
    
    * Fix round 2 of linting errors
    
    * Fix linting errors round 3
    
    * Fix linting errors round 4
    
    * Fix linting errors round 5
    
    * Fix missing comma in .eslintrc
    
    * Fix lint errors round 6
    
    * Fix lint errors round 7
    
    * Fix lint errors round 8
    
    * Alter regex for ignoring unused variables
    
    * Alter regex for ignoring unused variables round 2
    
    * Alter regex for ignoring unused variables round 3
    
    * Alter regex for ignoring unused variables round 4
    
    * Simplify regex for ignoring unused variables
    
    * Simplify regex for ignoring unused variables
    
    * Experimenting with marking update functions as exported
    
    * Moving exported statement to top of file in case it makes a difference
    
    * Speculating with marking functions as globals
    
    * Check whether no-unused-vars obeys .eslintrc
    
    * Testing no-unused-vars with varsIgnorePattern from docs
    
    * Put missing comma back into .eslintrc
    
    * Attempt to ignore variables containing 'update'
    
    * Attempt to refine ingore pattern to 'update*Data'
    
    * Attempt to ignore both 'update*Data' and 'updateHeader'
    
    * Have eslint no-unused-vars ignore all update functions
    
    * Have eslint no-unused-vars error instead of warn
    
    * Refer to the correct lib .js in index.js
    
    * Add node-report as an optionalDependency
    
    * Reflect new package name in test apps
    
    * Fix lint errors for flamegraph pass #1
    
    * Fix lint errors for flamegraph pass #2
    
    * Improve readability of varsIgnorePattern
    
    * Fix testing problems
    
    * Adding README badges
    
    * Update test html to include profiling
    
    * Fix linting errors resulting from translation
    
    * Fix linting errors
    
    * Last of the lint errors

--- js/nodeReport.js
+++ js/nodeReport.js
@@ -13,26 +13,27 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
-
-socket.on("nodereport", function(nodereport){
+socket.on('nodereport', function(nodereport){
   var newWindow = window.open();
   newWindow.document.open();
-    // XXX(sam) perhaps the errors should be formatted differently?
+  // XXX(sam) perhaps the errors should be formatted differently?
   nodereport = nodereport.error || nodereport.report;
-  newWindow.document.write("<html><head><title>NodeReport: "
-        + new Date().toUTCString()
-        + "</title></head><body><pre style=\"white-space: pre-wrap;\">"
-        + nodereport + "</pre></body></h‌​tml>");
+  newWindow.document.write('<html><head><title>NodeReport: '
+    + new Date().toUTCString()
+    + '</title></head><body><pre style="white-space: pre-wrap;">'
+    + nodereport + '</pre></body></h‌​tml>');
   newWindow.document.close();
 });
 
-socket.on("heapdump", function(info){
-  var text = "Heap snapshot generated at " + info.location;
+socket.on('heapdump', function(info){
+  var text = 'Heap snapshot generated at ' + info.location;
   if (info.error) {
-    text = "An error occurred: " + info.error;
+    text = 'An error occurred: ' + info.error;
   }
-  d3.select(".modal-body").select(".modaltext").remove();
-  d3.select(".modal-body").append("p")
-    .attr("class", "modaltext").text(text);
-  $("#heapdumpModal").modal("show");
+  d3.select('.modal-body').select('.modaltext').remove();
+  d3.select('.modal-body')
+    .append('p')
+    .attr('class', 'modaltext')
+    .text(text);
+  $('#heapdumpModal').modal('show');
 });
